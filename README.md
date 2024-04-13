b3039ea108b6d4ebf2132ad1a0aeb6760e1464fa
divikshrivastava <divikshrivastava@gmail.com>
2024-04-12 17:03:58 -0700

    Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <a href="https://demo.useliftoff.com">
-  <img alt="Liftoff – AI-Powered Mock Interviews" src="https://demo.useliftoff.com/opengraph-image">
-  <h1 align="center">Liftoff Interviews</h1>
+  <img alt="Mockerview – AI-Powered Mock Interviews" src="https://demo.useliftoff.com/opengraph-image">
+  <h1 align="center">Mockerview Interviews</h1>
 </a>
 
 <p align="center">
--- pages/demo.tsx
+++ pages/demo.tsx
@@ -56,7 +56,7 @@ export default function DemoPage() {
   const [selectedInterviewer, setSelectedInterviewer] = useState(
     interviewers[0]
   );
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
   const [loading, setLoading] = useState(true);
   const webcamRef = useRef<Webcam | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
@@ -718,7 +718,7 @@ export default function DemoPage() {
                   </motion.div>
                   <div className="flex flex-row space-x-4 mt-8 justify-end">
                     <button
-                      onClick={() => setStep(1)}
+                      onClick={() => setStep(0)}
                       className="group max-w-[200px] rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
                       style={{
                         boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
@@ -761,7 +761,75 @@ export default function DemoPage() {
           </motion.p>
           <div className="w-full min-h-[60vh] md:w-1/2 md:h-screen flex flex-col px-4 pt-2 pb-8 md:px-0 md:py-2 bg-[#FCFCFC] justify-center">
             <div className="h-full w-full items-center justify-center flex flex-col">
-              {step === 1 ? (
+              {step === 0 ? (<motion.div
+                  initial={{ opacity: 0, y: 40 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -40 }}
+                  key="step-0"
+                  transition={{
+                    duration: 0.95,
+                    ease: [0.165, 0.84, 0.44, 1],
+                  }}
+                  className="max-w-lg mx-auto px-4 lg:px-0"
+                >
+                  <h2 className="text-4xl font-bold text-[#1E2B3A]">
+                    Upload your resume
+                  </h2>
+                  <p className="text-[14px] leading-[20px] text-[#1a2b3b] font-normal my-4">
+                    Your resume helps us cater to specific areas that you are presenting to an actual interviewer.
+                  </p>
+                  <div>
+                    UPLOAD
+                  </div>
+                  <div className="flex gap-[15px] justify-end mt-8">
+                    <div>
+                      <Link
+                        href="/"
+                        className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
+                        style={{
+                          boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
+                        }}
+                      >
+                        Back to home
+                      </Link>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => {
+                          setStep(1);
+                        }}
+                        className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#1E2B3A] text-white hover:[linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0D2247] no-underline flex gap-x-2  active:scale-95 scale-100 duration-75"
+                        style={{
+                          boxShadow:
+                            "0px 1px 4px rgba(13, 34, 71, 0.17), inset 0px 0px 0px 1px #061530, inset 0px 0px 0px 2px rgba(255, 255, 255, 0.1)",
+                        }}
+                      >
+                        <span> Continue </span>
+                        <svg
+                          className="w-5 h-5"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M13.75 6.75L19.25 12L13.75 17.25"
+                            stroke="#FFF"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M19 12H4.75"
+                            stroke="#FFF"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </motion.div>): step === 1 ? (
                 <motion.div
                   initial={{ opacity: 0, y: 40 }}
                   animate={{ opacity: 1, y: 0 }}
@@ -910,16 +978,16 @@ export default function DemoPage() {
                     </RadioGroup>
                   </div>
                   <div className="flex gap-[15px] justify-end mt-8">
-                    <div>
-                      <Link
-                        href="/"
+                  <div>
+                      <button
+                        onClick={() => setStep(0)}
                         className="group rounded-full px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
                         style={{
                           boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
                         }}
                       >
-                        Back to home
-                      </Link>
+                        Previous step
+                      </button>
                     </div>
                     <div>
                       <button
@@ -1551,6 +1619,9 @@ export default function DemoPage() {
                 </ul>
               </div>
               <div className="bg-white text-[#667380] p-[18px] flex flex-col">
+                {
+                  step === 0 && (<></>)
+                }
                 {step === 1 ? (
                   <div>
                     <motion.span
